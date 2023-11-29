73a5398bbbeac471a51da0d2399ca679a7032791
MohuuStephen <mohuumaran100@gmail.com>
2023-11-29 13:42:33 +0530

    New Project is started

---
+++ src/HomePage.js
@@ -0,0 +1,33 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
+import './image.css'
+
+
+
+export const Homepage=()=>{
+    return(
+        <>
+        <div id="bc1">
+            <Navbar expand="lg" variant="light" >
+                <Container>
+                    <Navbar.Brand href="/" className="">JaiSuriya SuperMarket</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="manoj" />
+                    <Navbar.Collapse id="manoj">
+                        <Nav className="ms-5">   
+                        <NavDropdown title="ProductDetails" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="createproduct">Create</NavDropdown.Item>
+                        <NavDropdown.Item href="">ListAll(update & Read & Delete)</NavDropdown.Item>
+                        </NavDropdown>
+                        </Nav>
+                        <Nav className="ms-5">
+                        <button className="btn btn-outline-dark me-5 ">
+                                Logout
+                        </button>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            </div>
+        </>
+    )
+}
